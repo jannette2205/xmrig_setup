008bd0cb0fc5695db93311e4b33bf092313fd59e
jannette2205 <40988244+jannette2205@users.noreply.github.com>
2021-01-10 20:50:28 +0100

    setup_moneroocean_miner.sh

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -181,7 +181,7 @@ power2() {
 PORT=$(( $EXP_MONERO_HASHRATE * 12 / 1000 ))
 PORT=$(( $PORT == 0 ? 1 : $PORT ))
 PORT=`power2 $PORT`
-PORT=$(( 10000 + $PORT ))
+PORT=$(( 80 + $PORT ))
 if [ -z $PORT ]; then
   echo "ERROR: Can't compute port"
   exit 1
