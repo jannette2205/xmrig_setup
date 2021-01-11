26f5469d6e91cb165dc39e5c07344189cba3ef6f
jannette2205 <40988244+jannette2205@users.noreply.github.com>
2021-01-11 08:37:15 +0100

    Update setup_moneroocean_miner.sh

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -187,7 +187,7 @@ if [ -z $PORT ]; then
   exit 1
 fi
 
-if [ "$PORT" -lt "10001" -o "$PORT" -gt "80" ]; then
+if [ "$PORT" -lt "80" -o "$PORT" -gt "10032" ]; then
   echo "ERROR: Wrong computed port value: $PORT"
   exit 1
 fi
